7f887bbee24a74004cb2c4462cbfafcc3c16dbec
vatsal-dp <ptelvtsl@gmail.com>
2025-10-28 22:26:06 -0400

    login route fix for axios

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -30,7 +30,7 @@ const Login: React.FC = () => {
 
   const onSubmit = (values: ILoginFormValues, submitProps: FormikHelpers<ILoginFormValues>) => {
     axios
-      .post("http://localhost:3002/api/v1", values)
+      .post("http://localhost:3002/login", values)
       .then((response) => {
         const payload = setAuthToken(response.data.token);
 
